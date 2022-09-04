03f1f773f869717b2955350b0b9baa54040e29ed
Aaron Harmer <aharmer@gmail.com>
2022-09-05 08:18:52 +1200

    Updated writeConfig function to include exif_data from all cameras.

--- rapiid.py
+++ rapiid.py
@@ -106,10 +106,15 @@ class UI(QMainWindow):
         # Set initial camera variables and buttons
         self.liveView = False
         self.camera_type = None
-        self.camera_model = None
+        self.camera_1_model = None
+        self.camera_3_model = None
         self.file_format = ".jpg"
-        # self.DSLR_read_out = False
-        # self.ActiveSavingProcess = False
+
+        # Assign camera control features to ui
+        self.ui.pushButton_camera_1.pressed.connect(self.begin_live_view)
+        self.ui.spinBox_camera_1_exposure.valueChanged.connect(self.set_exposure_manual)
+        self.ui.doubleSpinBox_camera_1_gain.valueChanged.connect(self.set_gain_manual)
+        self.ui.doubleSpinBox_camera_1_gamma.valueChanged.connect(self.set_gamma)
 
         self.ui.pushButton_camera_3.pressed.connect(self.begin_live_view)
         self.ui.spinBox_camera_3_exposure.valueChanged.connect(self.set_exposure_manual)
@@ -122,25 +127,44 @@ class UI(QMainWindow):
         try:
             from scripts.rapiid_FLIR import customFLIR
             self.FLIR = customFLIR()
-            # camera needs to be initialised before use (self.cam.initialise_camera)
-            # all detected FLIR cameras are listed in self.cam.device_names
-            # by default, use the first camera found in the list
             self.cam = self.FLIR
-            self.cam.initialise_camera(select_cam=0)
-            # now retrieve the name of all found FLIR cameras and add them to the camera selection
-            # for cam in self.cam.device_names:
-            #     self.ui.comboBox_selectCamera.addItem(str(cam[0] + " ID: " + cam[1]))
-            self.camera_type = "FLIR"
-            # cam.device_names contains both model and serial number
-            self.camera_model = self.cam.device_names[0][0]
-            self.FLIR_found = True
-            self.FLIR_image_queue = []
-        except IndexError:
-            message = "No FLIR camera found!"
-            self.log_info(message)
-            print(message)
-            self.FLIR_found = False
-            self.disable_inputs()
+            
+            # Camera 1
+            try: 
+                self.cam.initialise_camera(select_cam = 0)
+                self.log_info("Dorsal camera successfully detected.")
+                self.ui.camera_1.setText("Dorsal camera successfully detected.")
+                self.camera_type = "FLIR"
+                # cam.device_names contains both model and serial number
+                self.camera_1_model = self.cam.device_names[0][0]
+                self.FLIR_found = True
+                self.FLIR_image_queue = []
+            except IndexError:
+                message1 = "Dorsal camera not detected!"
+                self.log_info(message1)
+                self.ui.camera_1.setText("Dorsal camera not detected!")
+                print(message1)
+                self.FLIR_found = False
+                self.disable_inputs()
+            
+            # Camera 3
+            try: 
+                self.cam.initialise_camera(select_cam = 1)
+                self.log_info("Label camera 1 successfully detected.")
+                self.ui.camera_3.setText("Label camera 1 successfully detected.")
+                self.camera_type = "FLIR"
+                # cam.device_names contains both model and serial number
+                self.camera_3_model = self.cam.device_names[1][0]
+                self.FLIR_found = True
+                self.FLIR_image_queue = []
+            except IndexError:
+                message = "Label camera 1 not detected!"
+                self.log_info(message3)
+                self.ui.camera_3.setText("Label camera 1 not detected!")
+                print(message3)
+                self.FLIR_found = False
+                self.disable_inputs()
+        
         except ModuleNotFoundError:
             message = "PYSPIN has not been installed - Disabling FLIR camera inputs"
             self.log_info(message)
@@ -154,9 +178,14 @@ class UI(QMainWindow):
         self.ui.pushButton_outputFolder.pressed.connect(self.set_output_location)
         self.output_location_folder = Path(self.output_location)
 
-        # use config file
+        # Config file
+        self.config = self.get_default_values()
+        self.ui.lineEdit_project.setText(self.config["general"]["project_name"])
         self.loadedConfig = False
         self.ui.pushButton_load_config.pressed.connect(self.loadConfig)
+        self.ui.pushButton_writeConfig.pressed.connect(self.writeConfig)
+
+        self.exif_data = self.config["exif_data"]
 
         # Show the app
         self.showMaximized()
@@ -276,7 +305,7 @@ class UI(QMainWindow):
 
     def loadConfig(self):
         file = QtWidgets.QFileDialog.getOpenFileName(self, "Load existing config file", str(Path.cwd()), "config file (*.yaml)")
-        config_location = file[0]
+        config_location = self.file[0]
         if config_location:
             # if a file has been selected, convert it into a Path object
             config_location = Path(config_location)
@@ -289,76 +318,76 @@ class UI(QMainWindow):
                                                "The selected config file was generated for a different camera type!")
                 return
 
-            # camera_settings:
+            # project name
+            self.ui.lineEdit_project.setText(config["general"]["project_name"])
 
+            # camera_settings:
             if config["general"]["camera_type"] == "FLIR":
                 # FLIR
-                self.ui.spinBox_camera_3_exposure.setValue(config["camera_settings"]["exposure_time"])
-                self.ui.doubleSpinBox_camera_3_gain.setValue(config["camera_settings"]["gain_level"])
-                self.ui.doubleSpinBox_camera_3_gamma.setValue(config["camera_settings"]["gamma"])
-                self.set_gamma()
+                self.ui.spinBox_camera_3_exposure.setValue(config["camera_settings"]["camera_3"]["exposure_time"])
+                self.ui.doubleSpinBox_camera_3_gain.setValue(config["camera_settings"]["camera_3"]["gain_level"])
+                self.ui.doubleSpinBox_camera_3_gamma.setValue(config["camera_settings"]["camera_3"]["gamma"])
+                # self.set_gamma()
 
             # meta data (exif)
-            self.exif = config["exif_data"]
+            self.exif_camera_3 = config["exif_data"]["camera_3"]
 
             self.loadedConfig = True
-            self.log_info("Loaded config-file successfully!")
+            self.log_info("Loaded config file successfully!")
 
             print(config)
 
-    # def writeConfig(self):
-    #     if self.ui.comboBox_stackingMethod.currentIndex() == 0:
-    #         stacking_method = "default"
-    #     elif self.ui.comboBox_stackingMethod.currentIndex() == 1:
-    #         stacking_method = "1-star"
-    #     elif self.ui.comboBox_stackingMethod.currentIndex() == 2:
-    #         stacking_method = "mask"
+    def writeConfig(self):
+        self.output_location_folder = Path(self.output_location).joinpath(self.ui.lineEdit_project.text())
+        if not os.path.exists(self.output_location_folder):
+            os.makedirs(self.output_location_folder)
+            self.log_info("Created folder: " + str(self.ui.lineEdit_project.text()))
 
-    #     if self.camera_type == "DSLR":
-    #         self.get_DSLR_file_ending()
+        config = {'general': {'project_name': self.ui.lineEdit_project.text(),
+                              'camera_type': self.camera_type,
+                              'camera_1_model': self.camera_1_model,
+                              'camera_3_model': self.camera_3_model,
+                              },
+                  'camera_settings': {'camera_1': {'exposure_time': self.ui.spinBox_camera_1_exposure.value(),
+                                                  'gain_level': self.ui.doubleSpinBox_camera_1_gain.value(),
+                                                  'gamma': self.ui.doubleSpinBox_camera_1_gamma.value(),
+                                                  },
+                                      'camera_3': {'exposure_time': self.ui.spinBox_camera_3_exposure.value(),
+                                                  'gain_level': self.ui.doubleSpinBox_camera_3_gain.value(),
+                                                  'gamma': self.ui.doubleSpinBox_camera_3_gamma.value(),
+                                                  }            
+                                      },
+                  'exif_data': self.exif_data
+                  }
 
-    #     config = {'general': {'project_name': self.ui.lineEdit_projectName.text(),
-    #                           'camera_type': self.camera_type,
-    #                           'camera_model': self.camera_model,
-    #                           },
-    #               'camera_settings': {'exposure_auto': self.ui.checkBox_exposureAuto.isChecked(),
-    #                                   'exposure_time': self.ui.doubleSpinBox_exposureTime.value(),
-    #                                   'gain_auto': self.ui.checkBox_gainAuto.isChecked(),
-    #                                   'gain_level': self.ui.doubleSpinBox_gainLevel.value(),
-    #                                   'gamma': self.ui.doubleSpinBox_gamma.value(),
-    #                                   'balance_ratio_red': self.ui.doubleSpinBox_balanceRatioRed.value(),
-    #                                   'balance_ratio_blue': self.ui.doubleSpinBox_balanceRatioBlue.value(),
-    #                                   # values specific to DSLR cameras
-    #                                   'shutterspeed': self.ui.comboBox_shutterSpeed.currentText(),
-    #                                   'aperture': self.ui.comboBox_aperture.currentText(),
-    #                                   'iso': self.ui.comboBox_iso.currentText(),
-    #                                   'whitebalance': self.ui.comboBox_whiteBalance.currentText(),
-    #                                   'compression': self.ui.comboBox_compression.currentText()
-    #                                   },
-    #               'scanner_settings': {'x_min': self.ui.doubleSpinBox_xMin.value(),
-    #                                    'x_max': self.ui.doubleSpinBox_xMax.value(),
-    #                                    'x_step': self.ui.doubleSpinBox_xStep.value(),
-    #                                    'y_min': self.ui.doubleSpinBox_yMin.value(),
-    #                                    'y_max': self.ui.doubleSpinBox_yMax.value(),
-    #                                    'y_step': self.ui.doubleSpinBox_yStep.value(),
-    #                                    'z_min': self.ui.doubleSpinBox_zMin.value(),
-    #                                    'z_max': self.ui.doubleSpinBox_zMax.value(),
-    #                                    'z_step': self.ui.doubleSpinBox_zStep.value()},
-    #               'stacking': {'stack_images': self.ui.checkBox_stackImages.isChecked(),
-    #                            'stacking_method': stacking_method,
-    #                            'threshold': self.ui.spinBox_thresholdFocus.value(),
-    #                            'display_focus_check': self.stackDisplayFocus,
-    #                            'additional_sharpening': self.stackSharpen},
-    #               'masking': {'mask_images': self.ui.checkBox_maskImages.isChecked(),
-    #                           'mask_thresh_min': self.ui.spinBox_thresholdMin.value(),
-    #                           'mask_thresh_max': self.ui.spinBox_thresholdMax.value(),
-    #                           'min_artifact_size_black': self.maskArtifactSizeBlack,
-    #                           'min_artifact_size_white': self.maskArtifactSizeWhite},
-    #               "exif_data": self.exif}
+        ymlRW.write_config_file(config, Path(self.output_location_folder))
+        self.log_info("Exported config file successfully!")
 
-    #     self.create_output_folders()
-    #     ymlRW.write_config_file(config, Path(self.output_location_folder))
-    #     self.log_info("Exported config_file successfully!")
+    def get_default_values(self):
+        # WARNING! THESE SETTINGS ARE SPECIFIC TO THE CAMERA USED DURING DEVELOPMENT
+        # OF THE SCANNER AND WILL LIKELY NOT APPLY TO YOUR SETUP
+        config = {'general': {'project_name': 'untitled_project'},
+                       'exif_data': {'camera_1': {'Make': 'FLIR',
+                                                  'Model': 'BFS-U3-200S6C-C',
+                                                  'SerialNumber': '21447407',
+                                                  'Lens': 'MPZ',
+                                                  'CameraSerialNumber': '21447407',
+                                                  'LensManufacturer': 'Computar',
+                                                  'LensModel': '35.0 f / 2.2',
+                                                  'FocalLength': '35.0',
+                                                  'FocalLengthIn35mmFormat': '95.0'},
+                                     'camera_3': {'Make': 'FLIR',
+                                                  'Model': 'BFS-U3-200S6C-C',
+                                                  'SerialNumber': '21188171',
+                                                  'Lens': 'MPZ',
+                                                  'CameraSerialNumber': '21188171',
+                                                  'LensManufacturer': 'Computar',
+                                                  'LensModel': '35.0 f / 2.2',
+                                                  'FocalLength': '35.0',
+                                                  'FocalLengthIn35mmFormat': '95.0'}
+                                    }
+                      }
+        return config
 
     
     def disable_inputs(self):
